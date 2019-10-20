1df60fbdb4af7c80b34b9e5f93f904c594866c39
Fernando <roflmaoermage@gmail.com>
2019-10-21 00:43:18 +0200

    he borrado algunos assets
    mañana mas y mejor

---
+++ cartitas/Assets/Resources/scripts/Card.cs
@@ -0,0 +1,47 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class Card : MonoBehaviour
+//{
+
+//    public int Cardvalue;
+//    public string Cardtype;
+//    public Image CardImage;
+//    public string Cardlocation;
+    
+
+//    public Card(int value, string type, Image image, string location)
+//    {
+//        this.Cardvalue = value;
+//        this.Cardtype = type;
+//        this.CardImage = image;
+//        this.Cardlocation = location;
+//    }
+
+//    public GameObject pepe;
+//    public Image[] CardImages;
+//    public List<Card> Deck; 
+    
+//    private void Start()
+//    {
+//     pepe.AddComponent(typeof(Card));
+
+//        //   Deck.Add(new Card(2, "tu", CardImages[1], "Deck"));
+//        //   Deck[1] = new Card(2, "tu", CardImages[1], "Deck");
+//        //  Deck[2] = new Card(6, "el", CardImages[2], "Deck");
+//        //   Deck[3] = new Card(9, "nostros", CardImages[3], "Deck");
+//        // Deck[4] = new Card(0, "vostros", CardImages[4], "Deck");
+
+
+//    }
+//    public void DrawCard()
+//    {
+        
+//        int RandomDraw = Random.Range(0, Deck.Count);
+        
+//        Deck.RemoveAt(RandomDraw);
+//    }
+
+//}
---
+++ cartitas/Assets/Resources/scripts/CardSprites.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardSprites : MonoBehaviour
+
+{
+    public Sprite[] EspaCardSprites;
+    public Image CartaDePrueba;
+    public Slider CartaDePruebaSlider; //lo que está debajo del botón de Cambiar carta, nose como se dice en español jeje 
+
+    public void Awake()    // el awake va antes del start y en este caso se encarga de que todos los sprites derivados de EspaCard se metan en el array EspaCardSprites
+    {                      // se puede ver este array una vez se le da al play en el objeto "las scripts"
+        EspaCardSprites = Resources.LoadAll<Sprite>("EspaCard");
+    }
+
+    public void cambiarfoto()
+    {
+
+        CartaDePrueba.sprite = EspaCardSprites[(int)CartaDePruebaSlider.value];  //cambia el sprite de la carta en función del valor del slider (está puesto de 0 a 50)
+
+    }
+
+}
